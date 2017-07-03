e2127417d577d5169f0490e4b005a467aa5d586b
Ben Carter <bencarter78@hotmail.com>
2017-07-03 22:05:05 +0100

    Setting up models and migrations

--- app/Stream.php
+++ app/Stream.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Objective extends Model
+class Stream extends Model
 {
     //
 }
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,11 +1,18 @@
 <?php
 
+$factory->define(App\Project::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->words(3, true),
+        'description' => $faker->paragraph,
+    ];
+});
+
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
         'name' => $faker->name,
-         'email' => $faker->unique()->safeEmail,
+        'email' => $faker->unique()->safeEmail,
         'password' => $password ?: bcrypt(config('trellis.demo.user.password')),
         'remember_token' => str_random(10),
         'api_token' => str_random(60),
